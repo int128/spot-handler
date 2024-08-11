6cd4ba67a5c00157c7ea2bddd256cc91385eaf4b
Hidetake Iwata <int128@gmail.com>
2024-08-11 11:27:01 +0900

    kubebuilder create api --group spothandler --version v1 --kind SpotInterruptedNode --namespaced=false

--- cmd/main.go
+++ cmd/main.go
@@ -161,6 +161,13 @@ func main() {
 		setupLog.Error(err, "unable to create controller", "controller", "SpotInterruptedPod")
 		os.Exit(1)
 	}
+	if err = (&controller.SpotInterruptedNodeReconciler{
+		Client: mgr.GetClient(),
+		Scheme: mgr.GetScheme(),
+	}).SetupWithManager(mgr); err != nil {
+		setupLog.Error(err, "unable to create controller", "controller", "SpotInterruptedNode")
+		os.Exit(1)
+	}
 	// +kubebuilder:scaffold:builder
 
 	if err := mgr.AddHealthzCheck("healthz", healthz.Ping); err != nil {
